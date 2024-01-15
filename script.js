8cd620a9d4318d2e41ec46c1fb5260cfc615777a
Adelina Moroaca <moroacaadelina@gmail.com>
2024-01-15 21:02:55 +0200

    Add events fot buttons

--- script.js
+++ script.js
@@ -28,9 +28,9 @@ function divideNumbers(a,b){
 console.log('divideNumbers', divideNumbers(17,15))
 
 //CREATE VARIABLES FOR EACH OF THE PARTS OF A CALCULATOR OPERATION
-// let firstNum = prompt('enter first number');
+let firstNum = document.getElementById('');
 let operator = document.getElementById('operator');
-// let secondNum = prompt('enter second number');
+let secondNum = document.getElementById('');
 
 //Create a new function operate that takes an operator and 2 numbers
 // and then calls one of the above functions on the numbers.
@@ -48,17 +48,41 @@ function operate(a, b, operator){
 
 console.log('operate', operate(10,10,'+'));
 
-// const calc = (a, b, operation) => {
-//     if (operation === '+'){
-//         return addNumbers(a,b);
-//     } else if(operation === '-'){
-//         return subtractNumbers(a,b);     
-//     } else if(operation === '*'){
-//         return multiplyNumbers(a,b);     
-//     } else if(operation === '/'){
-//         return divideNumbers(a,b);     
-//     }
-// }
+// Create the functions that populate the display when you click the number buttons. 
+// You should be storing the ‘display value’ in a variable somewhere for use
+// in the next step.
+let display = document.getElementById('display');
+let clearBtn = document.getElementById('clear');
+let deleteBtn = document.getElementById('delete');
+
+let opDivide = document.getElementById('operatorDivide');
+let opMultiply = document.getElementById('operatorMultiply');
+let opSubtract = document.getElementById('operatorSubtract');
+let opAdd = document.getElementById('operatorAdd');
+let btnPoint = document.getElementById('btnPoint');
+let opEqual = document.getElementById('operatorEqual');
 
 
-// console.log('calc', calc(10,10,'/'));
+let num9 = document.getElementById('btn9');
+let num8 = document.getElementById('btn8');
+let num7 = document.getElementById('btn7');
+let num6 = document.getElementById('btn6');
+let num5 = document.getElementById('btn5');
+let num4 = document.getElementById('btn4');
+let num3 = document.getElementById('btn3');
+let num2 = document.getElementById('btn2');
+let num1 = document.getElementById('btn1');
+let num0 = document.getElementById('btn0');
+
+let selectNum = '0';
+let displayValue = (e, num) => e.target.textContent = num.textContent;
+
+num9.addEventListener('click', displayValue);
+num8.addEventListener('click', displayValue);
+num7.addEventListener('click', displayValue);
+num6.addEventListener('click', displayValue);
+num5.addEventListener('click', displayValue);
+num4.addEventListener('click', displayValue);
+num3.addEventListener('click', displayValue);
+
+
