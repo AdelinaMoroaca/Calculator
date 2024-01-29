4728e33be5a72f2f1775f4c30d95499d85023eb1
Adelina Moroaca <moroacaadelina@gmail.com>
2024-01-29 21:25:55 +0200

    Fixing bug for divide operation

--- script.js
+++ script.js
@@ -1,3 +1,39 @@
+// Create the functions that populate the display when you click the number buttons. 
+// You should be storing the ‘display value’ in a variable somewhere for use
+// in the next step.
+let resultDisplay = document.getElementById('resultDisplay');
+// let display = document.getElementById('display');
+let clearBtn = document.getElementById('clear');
+let deleteBtn = document.getElementById('delete');
+const errorDivide = document.getElementsByClassName('divideBy0');
+
+let opDivide = document.getElementById('operatorDivide');
+let opMultiply = document.getElementById('operatorMultiply');
+let opSubtract = document.getElementById('operatorSubtract');
+let opAdd = document.getElementById('operatorAdd');
+let btnPoint = document.getElementById('btnPoint');
+let opEqual = document.getElementById('operatorEqual');
+
+let num9 = document.getElementById('btn9');
+let num8 = document.getElementById('btn8');
+let num7 = document.getElementById('btn7');
+let num6 = document.getElementById('btn6');
+let num5 = document.getElementById('btn5');
+let num4 = document.getElementById('btn4');
+let num3 = document.getElementById('btn3');
+let num2 = document.getElementById('btn2');
+let num1 = document.getElementById('btn1');
+let num0 = document.getElementById('btn0');
+
+
+//CREATE VARIABLES FOR EACH OF THE PARTS OF A CALCULATOR OPERATION
+let firstNum = '0';
+let secondNum = '';
+let operator = '';
+let flag = true;
+
+resultDisplay.innerHTML = firstNum; 
+
 // CREATING MATH FUNCTIONS FOR ALL OF THE BASIC MATH OPERATORS
 // ADD
 function addNumbers(firstNum, secondNum){
@@ -19,37 +55,51 @@ function divideNumbers(firstNum, secondNum){
     return Number(firstNum) / Number(secondNum);
 }
 
-console.log('divideNumbers', divideNumbers(17,15))
-
-//CREATE VARIABLES FOR EACH OF THE PARTS OF A CALCULATOR OPERATION
-let firstNum = '0';
-let secondNum = '';
-let operator = '';
-let flag = true;
-
 //Create a new function operate that takes an operator and 2 numbers
 // and then calls one of the above functions on the numbers.
 
 function operate(){
-    console.log('equal');
-    if (operator === '+'){
+    if (operator === '+' && firstNum !== '' && secondNum !== ''){
         resultDisplay.innerHTML = addNumbers(firstNum,secondNum);
-    } else if(operator === '-'){
-        resultDisplay.innerHTML = subtractNumbers(firstNum,secondNum);     
-    } else if(operator === '*'){
-        resultDisplay.innerHTML = multiplyNumbers(firstNum,secondNum);     
-    } else if(operator === '/'){
-        resultDisplay.innerHTML = divideNumbers(firstNum,secondNum);     
-    }
-    console.log('FINAL FIRSTNUM', firstNum);
-    console.log('operator', operator);
-    console.log('FINAL SECONDNUM', secondNum);
-    // flag = true;
+
+    } else if(operator === '-' && firstNum !== '' && secondNum !== ''){
+        resultDisplay.innerHTML = subtractNumbers(firstNum,secondNum); 
+
+    } else if(operator === '*' && firstNum !== '' && secondNum !== ''){
+        resultDisplay.innerHTML = multiplyNumbers(firstNum,secondNum);   
+
+    } else if(operator === '/' && firstNum !== '' && secondNum !== '0' && secondNum !== ''){
+        resultDisplay.innerHTML = divideNumbers(firstNum,secondNum).toFixed(3);
+
+    } else if(operator === '/' && secondNum === '0' && firstNum !== ''){
+        for (const divideBy0 of errorDivide) {
+           divideBy0.disabled = true;
+        }
+        resultDisplay.innerHTML = `Error! You can't divide by 0! Restart by pressing CLEAR button !`;     
+    
+    } else if(operator === '+' && firstNum !== '' && secondNum === ''){
+        secondNum = firstNum;
+        resultDisplay.innerHTML = addNumbers(firstNum,secondNum);
+
+    }  else if(operator === '-' && firstNum !== '' && secondNum === ''){
+        secondNum = firstNum;
+        resultDisplay.innerHTML = subtractNumbers(firstNum,secondNum);
+
+    } else if(operator === '*' && firstNum !== '' && secondNum === ''){
+        secondNum = firstNum;
+        resultDisplay.innerHTML = multiplyNumbers(firstNum,secondNum);
+        
+    } else if(operator === '/' && firstNum !== '' && secondNum === ''){
+        secondNum = firstNum;
+        resultDisplay.innerHTML = divideNumbers(firstNum,secondNum);
+    } 
+    // console.log('FINAL FIRSTNUM', firstNum);
+    // console.log('operator', operator);
+    // console.log('FINAL SECONDNUM', secondNum);
+    flag = true;
 }
-// console.log('operate', operate(10,120,'/'));
 
 let selectOperator = (e) => {
-
     flag = false;
     console.log("Flag: " , flag)
     operator = e.target.textContent;
@@ -57,33 +107,6 @@ let selectOperator = (e) => {
     resultDisplay.innerHTML = firstNum + operator + secondNum; 
     console.log('operator', operator);
 }
-
-// Create the functions that populate the display when you click the number buttons. 
-// You should be storing the ‘display value’ in a variable somewhere for use
-// in the next step.
-let resultDisplay = document.getElementById('resultDisplay');
-// let display = document.getElementById('display');
-let clearBtn = document.getElementById('clear');
-let deleteBtn = document.getElementById('delete');
-
-let opDivide = document.getElementById('operatorDivide');
-let opMultiply = document.getElementById('operatorMultiply');
-let opSubtract = document.getElementById('operatorSubtract');
-let opAdd = document.getElementById('operatorAdd');
-let btnPoint = document.getElementById('btnPoint');
-let opEqual = document.getElementById('operatorEqual');
-
-let num9 = document.getElementById('btn9');
-let num8 = document.getElementById('btn8');
-let num7 = document.getElementById('btn7');
-let num6 = document.getElementById('btn6');
-let num5 = document.getElementById('btn5');
-let num4 = document.getElementById('btn4');
-let num3 = document.getElementById('btn3');
-let num2 = document.getElementById('btn2');
-let num1 = document.getElementById('btn1');
-let num0 = document.getElementById('btn0');
-resultDisplay.innerHTML = firstNum; 
 
 let displayValue = (e) => {
     // if(firstNum === '0'){
@@ -116,15 +139,18 @@ let displayValue = (e) => {
     }
 }
 
-
 function clearData(){
     firstNum = '0';
+    secondNum = '';
+    for (const divideBy0 of errorDivide) {
+            divideBy0.disabled = false;
+
+    }
     resultDisplay.innerHTML = firstNum;
 }
 
 function deleteNum(){
     firstNum = firstNum.slice(0, -1);
-    // resultDisplay.innerHTML = firstNum;
     if(firstNum.length < 1){
         firstNum = '0';
         resultDisplay.innerHTML = firstNum;
@@ -153,13 +179,17 @@ opMultiply.addEventListener('click', selectOperator);
 opDivide.addEventListener('click', selectOperator);
 opEqual.addEventListener('click', operate);
 
+// Users should be able to string together several operations and 
+//get the right answer, with each pair of numbers being evaluated at a time. 
+//For example, 12 + 7 - 5 * 3 = should yield 42. 
+//An example of the behavior we’re looking for would be this student solution.
 
-//pas 1:store first number
-//pas 2:oprator
-// pas 3 :store second number : pt rezolvare pas 3 : se reface pasul 1
-
-//function oprate() is triggered when you pres '='
-//when operate() is called, update the display with the ‘solution’ to the operation(code that can populate the display)
-
-//first number = result
-//second num =number;
+// Your calculator should not evaluate more than a single pair of numbers 
+//at a time. Example: you press a number button (12), followed by an operator
+// button (+), a second number button (7), and finally a second operator 
+//button (-). 
+//Your calculator should then do the following: 
+//first, evaluate the first pair of numbers (12 + 7), 
+//second, display the result of that calculation (19), 
+//and finally, use that result (19) as the first number in your 
+//new calculation, along with the next operator (-).
